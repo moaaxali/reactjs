671b715e5d49011ab3e18f06907642bd1f1a5ed9
Maaz Ali <moaaxali@gmail.com>
2021-02-04 21:10:22 +0500

    Components and Props, Added Comment Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import TasteOfJSX from './TasteOfJSX/TasteOfJSX'
+// import TasteOfJSX from './TasteOfJSX/TasteOfJSX'
+import Comment from './Comment/Comment'
 import './App.css';
 
-const user = {
-  firstName: 'Maaz',
-  lastName: 'Ali',
-  favoriteFigure: 7,
-  hobbies: ['Soccer', 'Vollyball', 'Hiking', 'Camping', 'Swimming', 'Coding'],
-}
+// const user = {
+//   firstName: 'Maaz',
+//   lastName: 'Ali',
+//   favoriteFigure: 7,
+//   hobbies: ['Soccer', 'Vollyball', 'Hiking', 'Camping', 'Swimming', 'Coding'],
+// }
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
 
 function App() {
   return (
     <div className="App">
-      <TasteOfJSX user={user} />
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+      />
+      {/* <TasteOfJSX user={user} /> */}
     </div>
   );
 }
