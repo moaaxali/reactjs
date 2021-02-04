c92eaabf4760c2811bf3bb7a1554471c39f9c03a
Maaz Ali <moaaxali@gmail.com>
2021-02-05 01:36:02 +0500

    Converted Clock Component from Function to Class

--- src/StatefulComponent/Clock.js
+++ src/StatefulComponent/Clock.js
@@ -1,10 +1,14 @@
-const Clock = (props) => {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class Clock extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div >
+    );
+  }
 }
 
 export default Clock
