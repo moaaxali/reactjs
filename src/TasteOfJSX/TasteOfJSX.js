b4502444313487c14502d5a99990c0bb1b1e5ce5
Maaz Ali <moaaxali@gmail.com>
2021-02-04 08:18:51 +0500

    Refactoring existing code, nothing new added

--- src/TasteOfJSX/TasteOfJSX.js
+++ src/TasteOfJSX/TasteOfJSX.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import formatName from '../formatName/formatName'
 
 class TasteOfJSX extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <div>
--- src/formatName/formatName.js
+++ src/formatName/formatName.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const formatName = (user) => {
     return '#' + user.toUpperCase()
 }
