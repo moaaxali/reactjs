3bfd19b216e00411e91a09f387a200d051ae5eb3
Maaz Ali <moaaxali@gmail.com>
2021-02-08 01:28:37 +0500

    'Add keys to list' In React list without keys throw warning - you must specify keys with each item so that React may better keep track of items in the list.

--- src/ListKeys/List1.js
+++ src/ListKeys/List1.js
@@ -1,6 +1,10 @@
 const List1 = (props) => {
   const numbers = [1, 2, 3, 4, 5];
-  const listItems = numbers.map((number) => <li>{number}</li>);
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
   return (
     <ul>{listItems}</ul>
   );
