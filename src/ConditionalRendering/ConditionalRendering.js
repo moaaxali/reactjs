76e8f934ec15601eadff54c28e11fd4cb37040c7
Maaz Ali <moaaxali@gmail.com>
2021-02-07 14:42:47 +0500

    Conditional Rendering - Inline If with Logical && Operator

--- src/ConditionalRendering/ConditionalRendering.js
+++ src/ConditionalRendering/ConditionalRendering.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import InlineLogical from './InlineLogical';
 import LoginControl from './LoginControl'
 
 class ConditionalRendering extends Component {
   constructor(props) {
     super(props);
+    this.state = { unreadMessages: ["abc", "xyz", "lmn"] }
   }
 
   render() {
     return (
-      <LoginControl />
+      <div>
+        <LoginControl />
+        <InlineLogical unreadMessages={this.state.unreadMessages} />
+      </div>
     );
   }
 }
---
+++ src/ConditionalRendering/InlineLogical.js
@@ -0,0 +1,14 @@
+const InlineLogical = (props) => {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+      </h2>}
+    </div>
+  );
+}
+
+export default InlineLogical
