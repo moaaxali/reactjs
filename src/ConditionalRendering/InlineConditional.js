3061db70f65fb84b668584c9962f8adf74e6d5ce
Maaz Ali <moaaxali@gmail.com>
2021-02-07 19:42:26 +0500

    Inline If-else with conditional  for larger expressions

--- src/ConditionalRendering/InlineConditional.js
+++ src/ConditionalRendering/InlineConditional.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
 
 class InlineConditional extends Component {
   constructor(props) {
     super(props);
-    this.state = { isLoggedIn: true }
+    this.state = { isLoggedIn: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isLoggedIn: !this.state.isLoggedIn })
   }
 
   render() {
     const isLoggedIn = this.state.isLoggedIn;
     return (
       <div>
-        <p>
-          The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in!
-        </p>
+        <div>
+          <p>
+            The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in!
+          </p>
+        </div>
+        <div>
+          {isLoggedIn
+            ? <LogoutButton onClick={this.handleClick} />
+            : <LoginButton onClick={this.handleClick} />
+          }
+        </div>
       </div>
     );
   }
