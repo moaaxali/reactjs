ab0d71d5370509d6a48ad97c48c73d06d048b9ce
Maaz Ali <moaaxali@gmail.com>
2021-02-08 22:50:15 +0500

    Extracted ControlledComponent form Forms Component

---
+++ src/Forms/ControlledComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export class ControlledComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value })
+  }
+
+  handleSubmit(e) {
+    alert(`Hello, ${this.state.value}`)
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ControlledComponent
--- src/Forms/Forms.js
+++ src/Forms/Forms.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react'
+import ControlledComponent from './ControlledComponent'
 
 export class Forms extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: ''
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value })
-  }
-
-  handleSubmit(e) {
-    alert(`Hello, ${this.state.value}`)
-    e.preventDefault();
-  }
-
   render() {
     return (
       <div className="Component">
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+        <ControlledComponent />
       </div>
     );
   }
