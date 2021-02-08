4987db41ca38e1c77bd8381bb9c73bfebcaea0a3
Maaz Ali <moaaxali@gmail.com>
2021-02-08 22:44:02 +0500

    Controlled Components - controlled Forms Component

--- src/Forms/Forms.js
+++ src/Forms/Forms.js
@@ -2,25 +2,37 @@ import React, { Component } from 'react'
 
 export class Forms extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
+      value: ''
+    };
 
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value })
+  }
+
+  handleSubmit(e) {
+    alert(`Hello, ${this.state.value}`)
+    e.preventDefault();
   }
 
   render() {
     return (
-      <div>
-        <form>
+      <div className="Component">
+        <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" name="name" />
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Submit" />
         </form>
       </div>
-    )
+    );
   }
 }
 
