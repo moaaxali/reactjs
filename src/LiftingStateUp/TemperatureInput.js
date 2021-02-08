62aa5df5f348fc738737cc292444aa7d9926b762
Maaz Ali <moaaxali@gmail.com>
2021-02-09 02:21:19 +0500

    State lifted - Now the state of C & F updates asynchronously

--- src/LiftingStateUp/TemperatureInput.js
+++ src/LiftingStateUp/TemperatureInput.js
@@ -13,11 +13,11 @@ export class TemperatureInput extends Component {
   }
 
   handleChange(e) {
-    this.setState({ temperature: e.target.value });
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
       <fieldset>
