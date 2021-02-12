f61a50507992d3bc6faa66c02a85de2510f9329b
Maaz Ali <moaaxali@gmail.com>
2021-02-12 08:55:15 +0500

    Convert static HTML to React in ProductTable

--- src/FilterableProductTable/ProductTable.js
+++ src/FilterableProductTable/ProductTable.js
@@ -1,42 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function ProductTable() {
-  return (
-    <div>
+export class ProductTable extends Component {
+  render() {
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category} />
+        );
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name} />
+      );
+      lastCategory = product.category;
+    });
+
+    return (
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-        <tr colspan="2">Sporting Good</tr>
-        <tr>
-          <td>Football</td>
-          <td>$49.49</td>
-        </tr>
-        <tr>
-          <td>Baseball</td>
-          <td>$9.99</td>
-        </tr>
-        <tr>
-          <td>Basetball</td>
-          <td>$29.99</td>
-        </tr>
-        <tr colspan="2">Electronics</tr>
-        <tr>
-          <td>iPod Touch</td>
-          <td>$99.99</td>
-        </tr>
-        <tr>
-          <td>iPone5</td>
-          <td>$399.99</td>
-        </tr>
-        <tr>
-          <td>Nexus 7</td>
-          <td>$199.99</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
       </table>
-    </div>
-  )
+    )
+  }
 }
 
 export default ProductTable
