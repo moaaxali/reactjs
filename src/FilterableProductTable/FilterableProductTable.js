ef7e8b8e56e3cf6f91980f938bcb5744092905c7
Maaz Ali <moaaxali@gmail.com>
2021-02-12 09:23:40 +0500

    Identify, Add state to FilterProductTable

--- src/FilterableProductTable/FilterableProductTable.js
+++ src/FilterableProductTable/FilterableProductTable.js
@@ -3,12 +3,25 @@ import ProductTable from './ProductTable'
 import SearchBar from './SearchBar'
 
 export class FilterableProductTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { filterText: 'ball', inStockOnly: false };
+  }
+
   render() {
     return (
       <div>
         <div>
-          <SearchBar />
-          <ProductTable products={this.props.products} />
+          <SearchBar
+            filterText={this.state.filterText}
+            inStockOnly={this.state.inStockOnly}
+          />
+          <ProductTable
+            products={this.props.products}
+            filterText={this.state.filterText}
+            inStockOnly={this.state.inStockOnly}
+          />
         </div>
       </div>
     )
--- src/FilterableProductTable/SearchBar.js
+++ src/FilterableProductTable/SearchBar.js
@@ -1,10 +1,15 @@
-function SearchBar() {
+function SearchBar(props) {
   return (
     <div>
       <form>
-        <input type="text" placeholder="Search..." />
+        <input
+          type="text"
+          placeholder="Search..."
+          value={props.filterText} />
         <p>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={props.inStockOnly} />
           {' '}
           Only show products in stock
         </p>
