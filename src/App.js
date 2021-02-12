d80c6a0167ae509dae7a6c9d25dd6de3c56732c9
Maaz Ali <moaaxali@gmail.com>
2021-02-12 08:42:36 +0500

    Pass PRODUCTS as props to FilterableProductTable

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import TasteOfJSX from './TasteOfJSX/TasteOfJSX'
 import './App.css';
 import FilterableProductTable from './FilterableProductTable/FilterableProductTable';
 
+const PRODUCTS = [
+  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+];
+
 function App() {
   return (
     <div className="App">
       <TasteOfJSX user="maaz" />
-      <FilterableProductTable />
+      <FilterableProductTable product={PRODUCTS} />
     </div>
   );
 }
